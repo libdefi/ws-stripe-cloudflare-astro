c84a1863652ab657f59ee28cbefaac5df811420a
libdefi <joji.sekiya@gmail.com>
2023-05-06 22:03:30 +0900

    Create and embed a "fee schedule" in Stripe.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
-/// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly PUBLIC_STRIPE_PUBLISHABLE_API_KEY: string;
+    readonly PUBLIC_STRIPE_PRICING_TABLE_ID: string;
+    readonly PUBLIC_STIPE_CUSTOMER_PORTAL_URL: string;
+    // その他の環境変数...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
